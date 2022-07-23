5c876c8bda61030c156db1023cb764a918b2b3fb
Dheeraj <Dheeraj160199@gmail.com>
2022-07-23 23:48:30 +0530

    added git ignore + started creating tet and deployment scripts

---
+++ test/unit/Raffle.test.js
@@ -0,0 +1,35 @@
+const { assert, expect } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", function () {
+          let raffle, raffleContract, vrfCoordinatorV2Mock, raffleEntranceFee, interval, player // , deployer
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners() // could also do with getNamedAccounts
+              //   deployer = accounts[0]
+              player = accounts[1]
+              await deployments.fixture(["mocks", "raffle"]) // Deploys modules with the tags "mocks" and "raffle"
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock") // Returns a new connection to the VRFCoordinatorV2Mock contract
+              raffleContract = await ethers.getContract("Raffle") // Returns a new connection to the Raffle contract
+              raffle = raffleContract.connect(player) // Returns a new instance of the Raffle contract connected to player
+              raffleEntranceFee = await raffle.getEntranceFee()
+              interval = await raffle.getInterval()
+          })
+
+          describe("constructor", function () {
+              it("intitiallizes the raffle correctly", async () => {
+                  // Ideally, we'd separate these out so that only 1 assert per "it" block
+                  // And ideally, we'd make this check everything
+                  const raffleState = (await raffle.getRaffleState()).toString()
+                  // Comparisons for Raffle initialization:
+                  assert.equal(raffleState, "0")
+                  assert.equal(
+                      interval.toString(),
+                      networkConfig[network.config.chainId]["keepersUpdateInterval"]
+                  )
+              })
+          })
+      })
