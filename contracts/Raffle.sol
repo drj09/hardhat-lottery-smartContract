8590fc469d06503607720a1847afcd4e1ad20450
Dheeraj <Dheeraj160199@gmail.com>
2022-07-23 13:03:53 +0530

    added chainlink for random numbers

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,9 +2,11 @@
 
 pragma solidity ^0.8.9;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle_NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /*state variables*/
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -12,7 +14,7 @@ contract Raffle {
     /*events*/
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -25,7 +27,12 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    //functionpickRandomWinner(){}
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     /*view and pure function */
     function getEntranceFee() public view returns (uint256) {
