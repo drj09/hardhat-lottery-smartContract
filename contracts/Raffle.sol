346d0df3a8360632e5cb9a5955be1d808ab70588
Dheeraj <Dheeraj160199@gmail.com>
2022-07-23 12:56:47 +0530

    added events

---
+++ contracts/Raffle.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.9;
+
+error Raffle_NotEnoughETHEntered();
+
+contract Raffle {
+    /*state variables*/
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /*events*/
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffler() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEnter(msg.sender);
+    }
+
+    //functionpickRandomWinner(){}
+
+    /*view and pure function */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
