45a8dbd17ba4677a84e481eaf2ae899d5d656722
Dheeraj <Dheeraj160199@gmail.com>
2022-07-23 22:32:09 +0530

    created smart contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contractes/serc/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle_NotEnoughETHEntered();
 
@@ -10,12 +11,14 @@ contract Raffle is VRFConsumerBaseV2 {
     /*state variables*/
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
     /*events*/
     event RaffleEnter(address indexed player);
 
     constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     }
 
     function enterRaffler() public payable {
@@ -27,7 +30,15 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RaffleEnter(msg.sender);
     }
 
-    function requestRandomWinner() external {}
+    function requestRandomWinner() external {
+        i_vrfCoordinator.requestRandomWords(
+            keyHash, //gaslane
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
         internal
